64be9e22cfae5ceb454e7bca81e9391f52e52af0
Abdulwaarith Zakariyya <abdulwaarithz@gmail.com>
2022-10-13 23:11:11 +0100

    Adjusted the margin-top position for the contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -33,7 +33,7 @@ const Contact = () => {
   };
   return (
     <section id="contact">
-      <h5>Get IN Touch</h5>
+      <h5 className="h-5">Get In Touch</h5>
       <h2>Contact Me</h2>
 
       <div className="container contact_container">
